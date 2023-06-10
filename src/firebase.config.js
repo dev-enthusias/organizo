144581be2719395506b0247f35e9c1a0dd23a4a2
Odogwu <becomingadev@gmail.com>
2023-06-10 23:34:50 +0100

    App initial setup

---
+++ src/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDQ8kaIJvJNOqJ75VVXM4KM2EQp6YmBxXY',
+  authDomain: 'organizo-8d682.firebaseapp.com',
+  projectId: 'organizo-8d682',
+  storageBucket: 'organizo-8d682.appspot.com',
+  messagingSenderId: '446909641468',
+  appId: '1:446909641468:web:2de5242c7645f7e1405d79',
+  measurementId: 'G-964890VCJH',
+};
+
+const app = initializeApp(firebaseConfig);
