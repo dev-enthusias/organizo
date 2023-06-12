859a0c2a8d2361e3dc0f68b41351ab7ccbf01767
Odogwu <becomingadev@gmail.com>
2023-06-12 17:09:22 +0100

    Create Dashboard, Dashboard components and its layout

---
+++ src/components/Task.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Task({ className }) {
+  return <div className={className}>Task</div>;
+}
---
+++ src/components/Track.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Track({ className }) {
+  return <div className={className}>Tracking</div>;
+}
