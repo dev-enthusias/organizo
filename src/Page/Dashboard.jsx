5ca2d9eb2a5f8984134259a78a8232a32389e822
Odogwu <becomingadev@gmail.com>
2023-06-12 17:13:37 +0100

    Create the Dashboard, Dashboard component, and its layout

---
+++ src/Page/Dashboard.jsx
@@ -0,0 +1,23 @@
+import SearchBar from '../components/SearchBar';
+import Calendar from '../components/Calendar';
+import Task from '../components/Task';
+import Category from '../components/Category';
+import Track from '../components/Track';
+import Comment from '../components/Comment';
+import AddWidget from '../components/AddWidget';
+
+export default function Dashboard() {
+  return (
+    <div className='h-screen'>
+      <div className='h-screen p-2 grid grid-cols-9 grid-rows-[1fr_6fr_6fr] gap-3'>
+        <SearchBar className='col-start-1 col-end-10 border border-solid border-black' />
+        <Calendar className='col-start-1 col-span-3 border border-solid border-black' />
+        <Task className='col-start-4 col-span-4 border border-solid border-black' />
+        <Comment className='col-end-10 col-span-2 border border-solid border-black' />
+        <Category className='col-start-1 col-span-3 border border-solid border-black' />
+        <Track className='col-start-4 col-span-4 border border-solid border-black' />
+        <AddWidget className='col-end-10 col-span-2 border border-solid border-black' />
+      </div>
+    </div>
+  );
+}
